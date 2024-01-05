2d4cf457615d553f6d85033c41bda40dd4b0acfe
Diktator <dychervony@gmail.com>
2024-01-05 19:34:45 +0200

    Пофиксил формы в About Hotel. И да, Стас, переименуй лучше ты мою ветку, т.к. не хочу потом проблем с гитом.

--- Elements.js
+++ Elements.js
@@ -16,9 +16,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 const optionMenu = document.querySelector(".select-menu"),
-       selectBtn = optionMenu.querySelector(".select-btn"),
-       options = optionMenu.querySelectorAll(".option"),
-       sBtn_text = optionMenu.querySelector(".sBtn-text");
+selectBtn = optionMenu.querySelector(".select-btn"),
+options = optionMenu.querySelectorAll(".option"),
+sBtn_text = optionMenu.querySelector(".sBtn-text");
 
   selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
 
