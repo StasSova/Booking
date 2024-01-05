538f8c78cb0272272c8c9dc9f741a7eb3c4f9b9d
Diktator <dychervony@gmail.com>
2024-01-05 19:34:45 +0200

    Пофиксил формы в About Hotel. И да, Стас, переименуй лучше ты мою ветку, т.к. не хочу потом проблем с гитом.

--- Elements.js
+++ Elements.js
@@ -1,7 +1,7 @@
 const optionMenu = document.querySelector(".select-menu"),
-       selectBtn = optionMenu.querySelector(".select-btn"),
-       options = optionMenu.querySelectorAll(".option"),
-       sBtn_text = optionMenu.querySelector(".sBtn-text");
+selectBtn = optionMenu.querySelector(".select-btn"),
+options = optionMenu.querySelectorAll(".option"),
+sBtn_text = optionMenu.querySelector(".sBtn-text");
 
 selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
 
@@ -9,20 +9,12 @@ options.forEach(option =>{
     option.addEventListener("click", ()=>{
         let selectedOption = option.querySelector(".option-text").innerText;
         sBtn_text.innerText = selectedOption;
-
         optionMenu.classList.remove("active");
     });
 });
 document.getElementById('monthDayInput').addEventListener('input', function(e) {
     var input = e.target.value;
-  
-    if (input.length > 5) {
-      e.target.value = input.slice(0, 5);
-    }
-  
-    if (input.length === 2 && !input.includes('/')) {
-      e.target.value = input + '/';
-    }
-  
-  });
+    if (input.length > 5) e.target.value = input.slice(0, 5);
+    if (input.length === 2 && !input.includes('/')) e.target.value = input + '/';
+});
   
