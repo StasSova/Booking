a7d596e8e8e8f35d687343e95b368864ed3a02d8
StasSova <stassova2017@gmail.com>
2024-01-05 14:34:26 +0200

    Account. Сделал карточку юзера и форму для ввода данных

--- Elements.js
+++ Elements.js
@@ -20,16 +20,19 @@ const optionMenu = document.querySelector(".select-menu"),
        options = optionMenu.querySelectorAll(".option"),
        sBtn_text = optionMenu.querySelector(".sBtn-text");
 
-selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
+  selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
 
-options.forEach(option =>{
-    option.addEventListener("click", ()=>{
-        let selectedOption = option.querySelector(".option-text").innerText;
-        sBtn_text.innerText = selectedOption;
+  options.forEach(option => {
+      option.addEventListener("click", () => {
+          let selectedOption = option.querySelector(".option-text").innerText;
+          sBtn_text.innerText = selectedOption;
+          optionMenu.classList.remove("active");
+      });
+  });
+}
 
-        optionMenu.classList.remove("active");
-    });
-});
+
+
 document.getElementById('monthDayInput').addEventListener('input', function(e) {
     var input = e.target.value;
   
