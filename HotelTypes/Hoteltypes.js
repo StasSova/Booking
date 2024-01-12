26044b99dabd01fe55793456d63c5b0d3dc4b352
StasSova <stassova2017@gmail.com>
2024-01-12 13:22:30 +0200

    слияние 2

--- HotelTypes/Hoteltypes.js
+++ HotelTypes/Hoteltypes.js
@@ -1,13 +1,18 @@
 document.querySelectorAll('.accordion-toggle').forEach(button => {
   button.addEventListener('click', () => {
     const ratingDiv = button.nextElementSibling;
+
     ratingDiv.style.display = ratingDiv.style.display === 'none' ? 'block' : 'none';
+
     button.classList.toggle('active');
   });
 });
-  // Функция для переключения отображения section
+// Функция для переключения отображения section
 function toggleSection() {
-  var x = document.querySelector("section");
-  if (x.style.display === "none") x.style.display = "block";
-  else x.style.display = "none";
+var x = document.querySelector("section");
+if (x.style.display === "none") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
 }
