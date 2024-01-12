cce6b5726f656644e1a281bcd2755ccc288a9232
StasSova <stassova2017@gmail.com>
2024-01-12 13:43:16 +0200

    тест 3

--- Elements.js
+++ Elements.js
@@ -20,19 +20,16 @@ selectBtn = optionMenu.querySelector(".select-btn"),
 options = optionMenu.querySelectorAll(".option"),
 sBtn_text = optionMenu.querySelector(".sBtn-text");
 
-  selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
+selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
 
-  options.forEach(option => {
-      option.addEventListener("click", () => {
-          let selectedOption = option.querySelector(".option-text").innerText;
-          sBtn_text.innerText = selectedOption;
-          optionMenu.classList.remove("active");
-      });
-  });
-}
+options.forEach(option =>{
+    option.addEventListener("click", ()=>{
+        let selectedOption = option.querySelector(".option-text").innerText;
+        sBtn_text.innerText = selectedOption;
 
-
-
+        optionMenu.classList.remove("active");
+    });
+});
 document.getElementById('monthDayInput').addEventListener('input', function(e) {
     var input = e.target.value;
   
