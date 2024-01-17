ab5122e0ec9efb358be741c779c2e5934ca70ff9
Bor1sss <78493730+Bor1sss@users.noreply.github.com>
2024-01-17 16:45:29 +0200

    Добавил адаптив для отелей , и отображения сеткой но не получилось выровнять

---
+++ slider.js
@@ -0,0 +1,54 @@
+let currentSlide = 0;
+let isSliderEnabled = false;
+
+function showSlide(index) {
+  const slider = document.querySelector('.slider');
+  const slideWidth = document.querySelector('.sm-hotel-card').offsetWidth;
+  const newTransformValue = -index * slideWidth;
+  slider.style.transform = `translateX(${newTransformValue}px)`;
+  currentSlide = index;
+}
+
+function nextSlide() {
+  const totalSlides = document.querySelectorAll('.sm-hotel-card').length;
+  currentSlide = (currentSlide + 1) % totalSlides;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  const totalSlides = document.querySelectorAll('.sm-hotel-card').length;
+  currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+  showSlide(currentSlide);
+}
+
+function handleResize() {
+  const isMobile = window.matchMedia('(max-width: 950px)').matches;
+
+  if (isMobile && !isSliderEnabled) {
+    enableSlider();
+  } else if (!isMobile && isSliderEnabled) {
+    disableSlider();
+  }
+}
+
+function enableSlider() {
+  document.querySelector('.slider-container').style.display = 'block';
+  document.querySelector('.slider').style.transition = 'transform 2s ease-in-out';
+  setInterval(nextSlide, 3000);
+  isSliderEnabled = true;
+}
+
+function disableSlider() {
+  document.querySelector('.slider-container').style.display = 'none';
+  document.querySelector('.slider').style.transition = 'none';
+  clearInterval();
+  isSliderEnabled = false;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  handleResize(); // Вызовите при загрузке страницы для инициализации
+  window.addEventListener('resize', handleResize); // Обработчик изменения размера окна браузера
+});
+
+
+
